b41ec9cb56f5e7f81c39cf2e340cb9f553044f74
annapeterson89@gmail.com <annapeterson89@gmail.com>
2024-12-15 17:29:13 -0500

    render success nums for crits and messy crits

--- main.js
+++ main.js
@@ -193,10 +193,10 @@ function getFinalResult() {
       return "Bestial Fail!";
     }
     if ((crits && criticalHunger) || criticalHunger >= 2) {
-      return "Messy Crit!";
+      return `Messy Crit! (${successes})`;
     }
     if (crits >= 2) {
-      return "Crit Success!";
+      return `Crit Success! (${successes})`;
     }
     if (successes == 0) {
       return "Fail";
