2a7c54770f64da7a116e3b6975dcc799aedcba8d
annapeterson89@gmail.com <annapeterson89@gmail.com>
2024-12-15 17:49:24 -0500

    trying to make it look better

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ function renderResults() {
         default:
           image = "success-cat.png";
       }
-      resultsFinalInnerHTML = `<div class="vtm valign-wrapper"><span>${finalResult}</span><img class="vtm" src="imgs/${image}" alt="${finalResult}" ></div>`;
+      resultsFinalInnerHTML = `<div class="vtm valign-wrapper"><br />${finalResult}<img class="vtm" src="imgs/${image}" alt="${finalResult}" ></div>`;
       resultsFinalEl.style = "display: block;";
     }
   }
