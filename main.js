d6abd78d93d0741574d6339fb501498a5897e9e0
annapeterson89@gmail.com <annapeterson89@gmail.com>
2025-01-14 18:06:12 -0500

    updates for style and js, improves readability, background position, and creates classes for State and Dice.

--- main.js
+++ main.js
@@ -1,156 +1,270 @@
 // variables
-let diceObj, diceResults, finalResult;
-let canReroll = false;
-let isVtM = true;
+let diceObj, state;
 
-// constants
+// dice results
+const failure = "failure"
+const critical = "critical"
+const success = "success"
+const messyCrit = "messyCrit"
+const criticalHungerDie = "criticalHungerDie"
+const beastialFailure = "beastialFailure"
+// other consts
+const maxDicePerLine = 4
+const critMin = 2
+const maxRerollableDice = 3
+
+// dice
 const vtmDice = [
-  "critical",
-  "failure",
-  "failure",
-  "failure",
-  "failure",
-  "failure",
-  "success",
-  "success",
-  "success",
-  "success",
+  critical,
+  failure,
+  failure,
+  failure,
+  failure,
+  failure,
+  success,
+  success,
+  success,
+  success,
 ];
 const vtmHungerDice = [
-  "critical hunger",
-  "bestial failure",
-  "failure",
-  "failure",
-  "failure",
-  "failure",
-  "success",
-  "success",
-  "success",
-  "success",
+  criticalHungerDie,
+  beastialFailure,
+  failure,
+  failure,
+  failure,
+  failure,
+  success,
+  success,
+  success,
+  success,
 ];
-const submitBtn = document.getElementById("submit");
-const results = document.getElementById("results");
-const resultsFinalEl = document.getElementById("results-final");
-const burnAWillpower = document.getElementById("reroll-vtm");
-const resetBtn = document.getElementById("reset-vtm");
-const vtmArr = Array.from(document.getElementsByClassName("vtm"));
 
-// event listeners
-submitBtn.addEventListener("click", submit);
-burnAWillpower.addEventListener("click", rerollVTM);
-resetBtn.addEventListener("click", reset);
+// images
+const finalResultImgs = {
+  success: "success-cat.png",
+  failure: "fail-cat.png",
+  beastialFailure: "trash-cat.png",
+  critical: "crit-success-cat.png",
+  messyCrit: "kick-cat.png",
+};
+const diceResultImgs = {
+  failure: "fail.png",
+  success: "success.png",
+  criticalHungerDie: "beast.png",
+  beastialFailure: "grave-fail.png",
+  critical: "fang-icon.png"
+}
+
+// html elements
+const elements = {
+  submitBtn: document.getElementById("submit"),
+  results: document.getElementById("results"),
+  resultsFinalEl: document.getElementById("results-final"),
+  burnAWillpower: document.getElementById("reroll-vtm"),
+  resetBtn: document.getElementById("reset-vtm"),
+  vtmArr: Array.from(document.getElementsByClassName("vtm")),
+  dicePoolPicker: document.getElementById("dice-pool"),
+  hungerPicker: document.getElementById("hunger")
+}
+
+// classes
+class Dice {
+  constructor() {
+    this.hungerDice = {
+      qty: 1,
+    }
+    this.vtmDice = {
+      qty: 0,
+    }
+    this.diceResults = [];
+  }
+
+  setHunger(qty) {
+    this.hungerDice.qty = Number(qty);
+  }
+  setVTMDice(qty) {
+    this.vtmDice.qty = Number(qty)
+  }
+
+  // dice rolls
+  rollHungerDie() {
+    let r = Math.floor(Math.random() * vtmHungerDice.length);
+    return vtmHungerDice[r]
+  };
+  rollNormalDie() {
+    let r = Math.floor(Math.random() * vtmDice.length);
+    return vtmDice[r]
+  };
+
+  // rolls the dice and sets the diceResults array in this object
+  roll(dicePool) {
+    const totalDice = Number(dicePool)
+    if (totalDice <= 0) {
+      return []
+    }
+    let results = [];
+    for (let i = 0; i < totalDice; i++) {
+      if (i < this.hungerDice.qty) {
+        results[i] = this.rollHungerDie()
+        continue;
+      }
+      results[i] = this.rollNormalDie()
+      if (results[i] == failure) {
+        state.allowReroll(true)
+      }
+    }
+    this.diceResults = results;
+  };
+
+  // re-rolls up to 3 normal dice in the existing diceResults map and replaces the diceResults in this object with the new array
+  reroll() {
+    let times = 0
+    let result = this.diceResults.map((die, i) => {
+      if (i < this.hungerDice.qty || die != failure || times >= maxRerollableDice) {
+        return die;
+      }
+      times++
+      let newDie = this.rollNormalDie()
+      return newDie
+    });
+    this.diceResults = result
+  };
+
+  // dice results are an empty array again
+  reset() {
+    this.diceResults = []
+  }
+}
+
+class State {
+  constructor() {
+    this.successes = 0;
+    this.fails = 0;
+    this.crits = 0;
+    this.criticalHunger = 0;
+    this.beastialFailures = 0;
+    this.canReroll = false;
+  };
+
+  set(diceResults) {
+    let successes = 0;
+    let fails = 0;
+    let beastialFailures = 0;
+    let crits = 0;
+    let criticalHunger = 0;
+    diceResults.forEach((d) => {
+      switch (d) {
+        case success:
+          successes += 1;
+          break;
+        case failure:
+          fails += 1;
+          break;
+        case beastialFailure:
+          beastialFailures += 1;
+          break;
+        case critical:
+          crits += 1;
+          break;
+        case criticalHungerDie:
+          criticalHunger += 1;
+          break;
+      }
+    });
+    this.successes = successes;
+    this.fails = fails;
+    this.beastialFailures = beastialFailures;
+    this.crits = crits;
+    this.criticalHunger = criticalHunger;
+    if (this.fails === 0) {
+      this.canReroll = false;
+    }
+  }
+
+  allowReroll(bool) {
+    this.canReroll = bool;
+  };
+
+  reset() {
+    this.successes = 0;
+    this.fails = 0;
+    this.crits = 0;
+    this.criticalHunger = 0;
+    this.beastialFailures = 0;
+    this.canReroll = false;
+    this.hunger = 1;
+  }
+}
 
 // functions
 function render() {
-  finalResult = getFinalResult();
   renderResults();
-
-  if (canReroll) {
-    burnAWillpower.style = "display: inline-block;";
-    canReroll = false;
+  if (state.canReroll) {
+    elements.burnAWillpower.style = "display: inline-block;";
     return;
+  } else {
+    elements.burnAWillpower.style = "display: none;";
   }
-  burnAWillpower.style = "display: none;";
-  return;
-
-  submitBtn.style = "display: none;";
 }
 
 function init() {
-  diceResults = [];
-  canReroll = false;
-  diceObj = {
-    hungerDice: {
-      qty: 1,
-      structure: vtmHungerDice,
-    },
-    vtmDice: {
-      qty: 0,
-      structure: vtmDice,
-    },
-  };
+  diceObj = new Dice()
+  state = new State()
+  // event listeners
+  elements.submitBtn.addEventListener("click", handleSubmit);
+  elements.burnAWillpower.addEventListener("click", handleReroll);
+  elements.resetBtn.addEventListener("click", reset);
   render();
 }
 
 function renderResults() {
   let innerHTML = [];
   let resultsFinalInnerHTML = "";
-  if (isVtM) {
-    let image = "";
-    if (diceResults.length > 0) {
-      innerHTML = vtmDiceResultsInnerHTML();
-      switch (finalResult) {
-        case "success":
-          image = "success-cat.png";
-          break;
-        case "Fail":
-          image = "fail-cat.png";
-          break;
-        case "Bestial Fail!":
-          image = "trash-cat.png";
-          break;
-        case "Crit Success!":
-          image = "crit-success-cat.png";
-          break;
-        case "Messy Crit!":
-          image = "kick-cat.png";
-          break;
-        default:
-          image = "success-cat.png";
-      }
-      resultsFinalInnerHTML = `<div class="results-msg valign-wrapper"><div class="row"><div class="valign-wrapper">${finalResult}</div><img class="vtm" src="imgs/${image}" alt="${finalResult}" ></div></div>`;
-      resultsFinalEl.style = "display: block;";
-    }
+  let finalResult = getFinalResult();
+
+  if (diceObj.diceResults.length > 0) {
+    innerHTML = vtmDiceResultsInnerHTML();
+    let text = getTextForFinalResult(finalResult)
+    let image = finalResultImgs[finalResult]
+    resultsFinalInnerHTML = `<div class="results-msg valign-wrapper"><div class="row"><div class="valign-wrapper center-text">${text}</div><img class="vtm" src="imgs/${image}" alt="${text}" ></div></div>`;
+    elements.resultsFinalEl.style = "display: block;";
   }
-  resultsFinalEl.innerHTML = resultsFinalInnerHTML;
+
+  elements.resultsFinalEl.innerHTML = resultsFinalInnerHTML;
   results.innerHTML = innerHTML.join(" ");
 }
 
 // VTM FUNCTIONS
-
 function vtmDiceResultsInnerHTML() {
   let innerHTML = [];
-  innerHTML = diceResults.map((d, i) => {
-    let image = "";
+  innerHTML = diceObj.diceResults.map((d, i) => {
+    let image = diceResultImgs[d];
     let text = d;
     switch (d) {
-      case "failure":
-        image = "imgs/fail.png";
+      case criticalHungerDie:
+        text = critical;
         break;
-      case "success":
-        image = "imgs/success.png";
-        break;
-      case "critical hunger":
-        image = "imgs/beast.png";
-        text = "critical";
-        break;
-      case "bestial failure":
-        image = "imgs/grave-fail.png";
+      case beastialFailure:
         text = "bestial<br />failure";
         break;
-      case "critical":
-        image = "imgs/fang-icon.png";
     }
+
+    var dieClass = "dice"
     if (i < diceObj.hungerDice.qty) {
-      return `
-    <td class="vtm hunger-dice card col red accent-3 center-align" id="die-${i}-results">
-    <p class="vtm center-align">${text}</p><img src="${image}" alt="${d}" class="card-image center-align">
-    </td>
-    `;
+      dieClass = "red accent-3 hunger-dice"
     }
-    return `
-    <td class="vtm dice card col center-align" id="die-${i}-results">
-    <p class="vtm center-align">${text}</p><img src="${image}" alt="${d}" class="vtm card-image center-align">
-    </td>
-    `;
+    return `<td class="vtm ${dieClass} card col center-align" id="die-${i}-results">
+    <p class="vtm center-align">${text}</p><img src="imgs/${image}" alt="${d}" class="card-image center-align">
+    </td>`
   });
-  let spliceIndexes = [];
+
+  let rows = [];
   for (let i = 0; i < innerHTML.length; i++) {
-    if (!(i % 4) && i != 0) {
-      spliceIndexes.push(i);
+    if (!(i % maxDicePerLine) && i != 0) {
+      rows.push(i);
     }
   }
-  spliceIndexes.forEach((v, i) => {
+  rows.forEach((v, i) => {
     innerHTML.splice(v + i, 0, `<tr class="vtm">`);
   });
   results.style = "display: table-row-group;";
@@ -158,115 +272,78 @@ function vtmDiceResultsInnerHTML() {
 }
 
 function getFinalResult() {
-  if (isVtM) {
-    let successes = 0;
-    let fails = 0;
-    let crits = 0;
-    let criticalHunger = 0;
-    let bestialFailures = 0;
-    diceResults.forEach((d, i) => {
-      switch (d) {
-        case "success":
-          successes += 1;
-          break;
-        case "failure":
-          fails += 1;
-          break;
-        case "bestial failure":
-          bestialFailures += 1;
-          break;
-        case "critical":
-          crits += 1;
-          break;
-        case "critical hunger":
-          criticalHunger += 1;
-          break;
-      }
-    });
-    if ((crits == 1 && !criticalHunger) || (!crits && criticalHunger == 1)) {
-      successes += 1; // one pt added to successes when there is only one crit
-    }
-    if (!successes && !bestialFailures && !crits && !criticalHunger) {
+  if ((state.crits == 1 && !state.criticalHunger) || (!state.crits && state.criticalHunger == 1)) {
+    state.successes += 1; // one pt added to successes when there is only one crit
+  }
+  if (!state.successes && !state.beastialFailures && !state.crits && !state.criticalHunger) {
+    return failure;
+  }
+  if (!state.successes && state.beastialFailures && !state.crits && !state.criticalHunger) {
+    return beastialFailure;
+  }
+  if ((state.crits && state.criticalHunger) || state.criticalHunger >= critMin) {
+    return messyCrit;
+  }
+  if (state.crits >= critMin) {
+    return critical;
+  }
+  if (state.successes == 0) {
+    return failure;
+  }
+  return success;
+}
+
+function getTextForFinalResult(finalResult) {
+  switch (finalResult) {
+    case failure:
       return "Fail";
-    }
-    if (!successes && bestialFailures && !crits && !criticalHunger) {
+    case beastialFailure:
       return "Bestial Fail!";
-    }
-    if ((crits && criticalHunger) || criticalHunger >= 2) {
-      return `Messy Crit! +${successes}`;
-    }
-    if (crits >= 2) {
-      return `Crit Success! +${successes}`;
-    }
-    if (successes == 0) {
-      return "Fail";
-    }
-    return "Success: " + successes;
+    case messyCrit:
+      return `Messy Crit! +${state.successes}`;
+    case critical:
+      return `Crit Success! +${state.successes}`;
+    case success:
+      return `Success: ${state.successes}`;
+    default:
+      return `Success: ${state.successes}`;
   }
-  return "and I oop";
 }
 
-function rollVtmHungerDie() {
-  let r = Math.floor(Math.random() * diceObj.hungerDice.structure.length);
-  return diceObj.hungerDice.structure[r];
-}
-
-function rollVtmDie() {
-  let r = Math.floor(Math.random() * diceObj.vtmDice.structure.length);
-  return diceObj.vtmDice.structure[r];
-}
-
-function rollVtMDice(dicepool) {
-  let results = [];
-  let fails = 0;
-  for (let i = 0; i < dicepool; i++) {
-    if (i < diceObj.hungerDice.qty) {
-      results[i] = rollVtmHungerDie();
-      continue;
-    }
-    results[i] = rollVtmDie();
-    if (results[i] == "failure") {
-      fails++;
-    }
-  }
-  if (fails > 0) canReroll = true;
-  return results;
-}
-
-function rerollVTM() {
-  canReroll = false;
-  let times = 0;
-  diceResults = diceResults.map((die, i) => {
-    if (i < hunger.value || die != "failure" || times >= 3) {
-      return die;
-    }
-    times++;
-    return rollVtmDie();
-  });
+function handleReroll() {
+  diceObj.reroll()
+  state.set(diceObj.diceResults)
+  state.allowReroll(false);
   render();
 }
 
 function reset() {
-  diceResults = [];
+  diceObj.reset()
+  state.reset()
   render();
 }
 
-function submit() {
-  if (isVtM) {
-    let dicepool = document.getElementById("dice-pool").value;
-    let hunger = document.getElementById("hunger").value;
-    diceObj = {
-      hungerDice: {
-        qty: hunger,
-        structure: vtmHungerDice,
-      },
-      vtmDice: {
-        qty: dicepool - hunger,
-        structure: vtmDice,
-      },
-    };
-    diceResults = rollVtMDice(dicepool);
+function handleSubmit() {
+  let dicePool = Number(elements.dicePoolPicker.value);
+  if (dicePool < 0) {
+    dicePool = 0
   }
+
+  let hunger = Number(elements.hungerPicker.value);
+  if (hunger < 0) {
+    hunger = 0
+  }
+
+  let vtmDice = dicePool - hunger;
+  if (vtmDice < 0) {
+    vtmDice = 0
+  }
+
+  // set state and update Dice
+  diceObj.setHunger(hunger)
+  diceObj.setVTMDice(vtmDice)
+  diceObj.roll(dicePool)
+  state.set(diceObj.diceResults);
   render();
 }
 
